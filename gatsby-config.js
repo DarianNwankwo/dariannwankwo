db3b73d4385a33eca1a2d1115a205e2065089750
Darian Nwankwo <nwankwodarian@gmail.com>
2020-07-29 19:55:30 -0400

    integrating github api

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,16 +8,26 @@ module.exports = {
   /* Your site config here */
   siteMetadata: {
     title: `self darian`,
+    twitterUsername: `darian_ai`,
+    instagramUsername: `darian.ai`,
+    url: `http://www.dariannwankwo.com`
   },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-remark`,
     `gatsby-plugin-svgr-svgo`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown`,
         path: `${__dirname}/src/markdown`,
+      }
+    },
+    {
+      resolve: `gatsby-source-github-api`,
+      options: {
+        url: `https://api.github.com/graphql`,
       }
     },
   ],
