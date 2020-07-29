e33f2434a7b2d9c12bd7620805f4efc781e4e9c0
Darian Nwankwo <nwankwodarian@gmail.com>
2020-07-29 01:10:39 -0400

    projects desktop view implemented

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Personal website built via Gatsby + React while referencing my XD mockup.
 - [ ] MobileLayout needs to be refactored into a class component.
   - Maintain the state of the mobile view through the MobileLayout component to adjust the view on different actions
 - [ ] Implement Hire Me page from the XD design
-- [ ] Implement Projects page from the XD design
+- [x] Implement Projects page from the XD design
+  - [ ] Implement logic to query github for projects
 - [ ] Implement Blog index page from the XD design
 - [ ] Leverage Gatsby's createPage API to dynamically render blog posts
 - [x] Create the template view component for blog posts
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 import ArrowSvg from "../../static/arrow.svg";
+import DropdownSvg from "../../static/dropdown.svg";
 import PlaceholderImage from "../../static/placeholder.png";
-// import PlaceholderImage from "../../static/me_2.jpg";
 import Card from "../Card";
+
 
 const ProjectsContainer = styled.div`
   // border: 2px solid red;
@@ -16,12 +17,26 @@ const ProjectsContainer = styled.div`
   justify-content: center;
 `
 
+const LeftPulsate = keyframes`
+  0% { left: 10px; }
+  50% { left: 13px}
+  100% { left: 10px; }
+`
+
+const RightPulsate = keyframes`
+  0% { right: 10px; }
+  50% { right: 13px}
+  100% { right: 10px; }
+`
+
 const LeftArrowContainer = styled.div`
   // border: 2px solid pink;
   position: absolute;
   top: 50%;
   left: 10px;
   transform: translateY(-50%);
+  transition: 0.5s;
+  animation: ${LeftPulsate} 700ms linear infinite;
 `
 
 const RightArrowContainer = styled.div`
@@ -30,6 +45,8 @@ const RightArrowContainer = styled.div`
   top: 50%;
   right: 10px;
   transform: translateY(-50%) rotate(180deg);
+  transition: 0.5s;
+  animation: ${RightPulsate} 700ms linear infinite;
 `
 
 const ImageContainer = styled.div`
@@ -46,21 +63,32 @@ const ProjectDescriptionContainer = styled.div`
   flex-shrink: 1;
   // border: 2px solid pink;
   display: flex;
-  flex-basis: 1px;
   align-items: center;
+  flex-basis: 1px;
 `
 
 const ShowAllContainer = styled.div`
-  flex-grow: 1;
+  // flex-grow: .5;
   flex-shrink: 1;
   // border: 2px solid pink;
 `
 
-const Image = styled.img`
-  max-width: 80%;
-  max-height: 80%;
+const DropDownMenu = styled.img`
   margin: 0 auto;
   display: block;
+  width: 50%;
+  transition: .1s;
+
+  &:hover {
+    width: 55%;
+  }
+`
+
+const Image = styled.img`
+  max-width: 70%;
+  // max-height: 80%;
+  margin: 0 auto;
+  // display: block;
   // border: 2px solid green;
   border-radius: 8px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5);
@@ -71,28 +99,69 @@ const Image = styled.img`
   }
 `
 
+const ProjectDescription = styled.div`
+  // border: 2px solid blue;
+  // background: red;
+  margin: 0 auto;
+  width: 75%;
+  height: 75%;
+  overflow: auto;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5);
+  transition: 0.3s;
+
+  &:hover {
+    box-shadow: 0px 8px 16px 0 rgba(255, 165, 0, 0.5);
+  }
+`
+
+const SpacedDiv = styled.div`
+  padding: 2% 5%;
+  // display: flex;
+  // border: 2px solid red;
+`
 
 
 class ProjectsPage extends Component {
   constructor() {
     super();
+    this.handleArrowClick = this.handleArrowClick.bind(this);
+  }
+  
+  handleArrowClick() {
+    alert("Clicked...")
   }
 
   render() {
     return (
       <ProjectsContainer>
-        <LeftArrowContainer>
-          <img src={ArrowSvg} />
+        <LeftArrowContainer onClick={this.handleArrowClick}>
+          <img src={ArrowSvg} width="75%" />
         </LeftArrowContainer>
         <ImageContainer>
           <Image src={PlaceholderImage} />
         </ImageContainer>
-        <ShowAllContainer></ShowAllContainer>
+        <ShowAllContainer>
+          <DropDownMenu src={DropdownSvg} />
+        </ShowAllContainer>
         <ProjectDescriptionContainer>
-          <Card width="80%" />
+          <ProjectDescription>
+            <SpacedDiv>
+              <h1>Project Title</h1>
+              <p>
+                Project description is right below the project title. It shows this following text because
+                I do not want to use lorem ipsum.
+              </p>
+              <h1>Developer Stack</h1>
+              <ul>
+                <li>Python</li>
+                <li>Javascript</li>
+              </ul>
+            </SpacedDiv>
+          </ProjectDescription>
         </ProjectDescriptionContainer>
         <RightArrowContainer>
-          <img src={ArrowSvg} />
+          <img src={ArrowSvg} width="75%" />
         </RightArrowContainer>
       </ProjectsContainer>
     )
